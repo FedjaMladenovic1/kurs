f2ac65a8cbed980f91b8773aec8b182e3aa23fc9
Fedja <fm20180328@student.fon.bg.ac.rs>
2023-08-04 12:22:27 +0200

    navbar

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>NBA</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Novi blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
